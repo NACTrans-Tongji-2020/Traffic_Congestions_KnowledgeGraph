c52dfe563e54d9983d085bc2edb491f1c5ed972e
ChocolateDave <45930715+ChocolateDave@users.noreply.github.com>
2020-01-24 01:22:15 +0800

    Renew file structure
    Fixing some issues

--- Traffic_Congestions_KnowledgeGraph/WeiboCrawler/WeiboCrawler/__init__.py
+++
@@ -1,9 +0,0 @@
-# !/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from . import items
-from . import middlewares
-from . import pipelines
-from . import settings
-
-__all__ = ['items','middelwares','piplines','settings']
---
+++ WeiboCrawler/WeiboCrawler/__init__.py
@@ -0,0 +1,9 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from . import items
+from . import middlewares
+from . import pipelines
+from . import settings
+
+__all__ = ['items','middelwares','piplines','settings']
